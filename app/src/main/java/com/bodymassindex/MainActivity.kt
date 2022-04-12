033168ce9c3d354d64be5b1a9ce76cc8dfd645e5
LF <luism.falconi@gmail.com>
2022-04-12 09:28:18 -0300

    Edit class MainActivity

--- app/src/main/java/com/bodymassindex/MainActivity.kt
+++ app/src/main/java/com/bodymassindex/MainActivity.kt
@@ -2,10 +2,26 @@ package com.bodymassindex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonCalculate.setOnClickListener {
+
+            textViewResult.text = calculatorBodyMassIndex().toString()
+        }
+
+    }
+    private fun calculatorBodyMassIndex(): Float {
+
+        val editWeight: Float = editTextWeightKilograms.text.toString().toFloat()
+        val editHeight: Float = editTextHeightCentimeters.text.toString().toFloat()
+
+        return (editWeight / (editHeight.pow(2)))
     }
 }
